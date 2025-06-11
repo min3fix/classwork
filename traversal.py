645a59b211ec45304097b65d5649029b87ca0e53
min3fix <louis.mwathi@strathmore.edu>
2025-06-11 10:51:51 +0300

    new file:   traversal.py

---
+++ traversal.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.value = value
+    def preorder_traversal(self):
+        pass
+    def insert(self,keyvalue):
+        pass
+    def find(self):
+        pass
+     
